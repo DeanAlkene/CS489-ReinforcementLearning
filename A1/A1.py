6c7308a648556bde80219f9e89e42b39444a438d
DeanAlkene <deanlhz@sjtu.edu.cn>
2020-03-28 22:39:54 +0800

    A1 Coding...

--- A1/A1.py
+++ A1/A1.py
@@ -1,16 +1,22 @@
-class MDP:
-    def __init__(self, state, action, trans, reward, gamma, policy, threshold):
+import math
+
+class GridWorld:
+    def __init__(self, state, terminalState, gamma, threshold):
         self.state = state #S
-        self.terminalState = []
-        self.action = action #A
-        self.trans = trans #P
-        self.reward = reward #R
+        self.terminalState = terminalState
         self.gamma = gamma #gamma
-        self.policy = policy #pi
         self.threshold = threshold #theta in IPE
 
-    def setTerminal(self, termList):
-        self.terminalState = termList
+        self.action = {'n' : 0, 'e' : 1, 's' : 2, 'w' : 3} #A
+        self.trans = [[0 for j in range(len(action))] for i in range(state)] #s' = trans[s][a] 
+        self.prob = [[[0 for k in range(state)] for j in range(len(action))] for i in range(state)] #P[s][a][s']
+        self.reward = [[-1 for j in range(len(action))] for i in range(state)] #E[R[s][a]]
+        self.policy = [[0.25 for j in range(len(action))] for i in range(state)] #pi
+        self.map = [[(i+1)*(j+1)-1 for j in range(int(math.sqrt(state)))] for i in range(int(math.sqrt(state)))]
+        self.__calcParam()
+
+    def __calcParam(self):
+        
 
     def evaluation(self):
         pass
@@ -28,10 +34,7 @@ class MDP:
         pass
 
 def main():
-    action = {'n' : 0, 'e' : 1, 's' : 2, 'w' : 3}
-    policy = []
-    gridWorld = MDP()
-    gridWorld.setTerminal([1, 35])
+    gridWorld = GridWorld(state=36, terminalState=[1,35], gamma=1, threshold=0.0001)
 
 
 if __name__ == '__main__':
