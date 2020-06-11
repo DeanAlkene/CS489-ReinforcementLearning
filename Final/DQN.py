e51a705a5a884c09059c8a181fad06b9cd197bf9
DeanAlkene <deanlhz@sjtu.edu.cn>
2020-06-11 17:32:04 +0800

    Speed Up DQN, thanks to OpenAI's baselines' code

--- Final/DQN.py
+++ Final/DQN.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from collections import namedtuple
+from collections import namedtuple, deque
 from itertools import count
 import random
 import math
 import gym
+from gym import spaces
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -13,16 +14,185 @@ import torch.nn.functional as F
 from torchvision import transforms as T
 from PIL import Image
 import time
+import cv2
+cv2.ocl.setUseOpenCL(False)
 
 Exp = namedtuple('Exp', ('state', 'action', 'reward', 'nextState', 'done'))
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 resize = T.Compose([T.ToPILImage(),
-                    T.Resize((84, 84), interpolation=Image.CUBIC),
                     T.ToTensor()])
-# device = "cpu"
+
+class NoopResetEnv(gym.Wrapper):
+    def __init__(self, env, noop_max=30):
+        gym.Wrapper.__init__(self, env)
+        self.noop_max = noop_max
+        self.override_num_noops = None
+        self.noop_action = 0
+        assert env.unwrapped.get_action_meanings()[0] == 'NOOP'
+
+    def reset(self, **kwargs):
+        self.env.reset(**kwargs)
+        if self.override_num_noops is not None:
+            noops = self.override_num_noops
+        else:
+            noops = self.unwrapped.np_random.randint(1, self.noop_max + 1) #pylint: disable=E1101
+        assert noops > 0
+        obs = None
+        for _ in range(noops):
+            obs, _, done, _ = self.env.step(self.noop_action)
+            if done:
+                obs = self.env.reset(**kwargs)
+        return obs
+
+    def step(self, ac):
+        return self.env.step(ac)
+
+class MaxAndSkipEnv(gym.Wrapper):
+    def __init__(self, env, skip=4):
+        gym.Wrapper.__init__(self, env)
+        # most recent raw observations (for max pooling across time steps)
+        self._obs_buffer = np.zeros((2,)+env.observation_space.shape, dtype=np.uint8)
+        self._skip       = skip
+
+    def step(self, action):
+        total_reward = 0.0
+        done = None
+        for i in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            if i == self._skip - 2: self._obs_buffer[0] = obs
+            if i == self._skip - 1: self._obs_buffer[1] = obs
+            total_reward += reward
+            if done:
+                break
+        # Note that the observation on the done=True frame
+        # doesn't matter
+        max_frame = self._obs_buffer.max(axis=0)
+
+        return max_frame, total_reward, done, info
+
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+
+class WarpFrame(gym.ObservationWrapper):
+    def __init__(self, env, width=84, height=84, grayscale=True, dict_space_key=None):
+        super().__init__(env)
+        self._width = width
+        self._height = height
+        self._grayscale = grayscale
+        self._key = dict_space_key
+        if self._grayscale:
+            num_colors = 1
+        else:
+            num_colors = 3
+
+        new_space = gym.spaces.Box(
+            low=0,
+            high=255,
+            shape=(self._height, self._width, num_colors),
+            dtype=np.uint8,
+        )
+        if self._key is None:
+            original_space = self.observation_space
+            self.observation_space = new_space
+        else:
+            original_space = self.observation_space.spaces[self._key]
+            self.observation_space.spaces[self._key] = new_space
+        assert original_space.dtype == np.uint8 and len(original_space.shape) == 3
+
+    def observation(self, obs):
+        if self._key is None:
+            frame = obs
+        else:
+            frame = obs[self._key]
+
+        if self._grayscale:
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        frame = cv2.resize(
+            frame, (self._width, self._height), interpolation=cv2.INTER_AREA
+        )
+        if self._grayscale:
+            frame = np.expand_dims(frame, -1)
+
+        if self._key is None:
+            obs = frame
+        else:
+            obs = obs.copy()
+            obs[self._key] = frame
+        return obs
+
+class FrameStack(gym.Wrapper):
+    def __init__(self, env, k):
+        gym.Wrapper.__init__(self, env)
+        self.k = k
+        self.frames = deque([], maxlen=k)
+        shp = env.observation_space.shape
+        self.observation_space = spaces.Box(low=0, high=255, shape=(shp[:-1] + (shp[-1] * k,)), dtype=env.observation_space.dtype)
+
+    def reset(self):
+        ob = self.env.reset()
+        for _ in range(self.k):
+            self.frames.append(ob)
+        return self._get_ob()
+
+    def step(self, action):
+        ob, reward, done, info = self.env.step(action)
+        self.frames.append(ob)
+        return self._get_ob(), reward, done, info
+
+    def _get_ob(self):
+        assert len(self.frames) == self.k
+        return LazyFrames(list(self.frames))
+
+class ScaledFloatFrame(gym.ObservationWrapper):
+    def __init__(self, env):
+        gym.ObservationWrapper.__init__(self, env)
+        self.observation_space = gym.spaces.Box(low=0, high=1, shape=env.observation_space.shape, dtype=np.float32)
+
+    def observation(self, observation):
+        return np.array(observation).astype(np.float32) / 255.0
+
+class LazyFrames(object):
+    def __init__(self, frames):
+        self._frames = frames
+        self._out = None
+
+    def _force(self):
+        if self._out is None:
+            self._out = np.concatenate(self._frames, axis=-1)
+            self._frames = None
+        return self._out
+
+    def __array__(self, dtype=None):
+        out = self._force()
+        if dtype is not None:
+            out = out.astype(dtype)
+        return out
+
+    def __len__(self):
+        return len(self._force())
+
+    def __getitem__(self, i):
+        return self._force()[i]
+
+    def count(self):
+        frames = self._force()
+        return frames.shape[frames.ndim - 1]
+
+    def frame(self, i):
+        return self._force()[..., i]
+
+def getEnv(env_id):
+    env = gym.make(env_id)
+    assert 'NoFrameskip' in env.spec.id
+    env = NoopResetEnv(env, noop_max=30)
+    env = MaxAndSkipEnv(env, skip=4)
+    env = WarpFrame(env)
+    # env = ScaledFloatFrame(env)
+    env = FrameStack(env, 4)
+    return env
 
 class Agent:
-    def __init__(self, actionSpace, observationSpace, gamma, epsilon, tau, batchSize, lr, hiddenSize, updateStride, maxStep):
+    def __init__(self, actionSpace, observationSpace, gamma, epsilon, tau, batchSize, lr, hiddenSize, learnStride, updateStride, maxStep, isDueling=False):
         self.actionSpace = actionSpace
         self.actionNum = actionSpace.n
         self.actionSpace = [i for i in range(self.actionNum)]
@@ -36,12 +206,17 @@ class Agent:
         self.batchSize = batchSize
         self.lr = lr
         self.hiddenSize = hiddenSize
+        self.learnStride = learnStride
         self.updateStride = updateStride
         self.maxStep = maxStep
         # self.net = DQN(self.stateSize[0], self.stateSize[1], self.stateSize[2], self.hiddenSize, self.actionNum).to(device)
         # self.targetNet = DQN(self.stateSize[0], self.stateSize[1], self.stateSize[2], self.hiddenSize, self.actionNum).to(device)
-        self.net = DQN(84, 84, 3, self.hiddenSize, self.actionNum).to(device)
-        self.targetNet = DQN(84, 84, 3, self.hiddenSize, self.actionNum).to(device)
+        if isDueling:
+            self.net = DuelingDQN(84, 84, 4, self.hiddenSize, self.actionNum).to(device)
+            self.targetNet = DuelingDQN(84, 84, 4, self.hiddenSize, self.actionNum).to(device)
+        else:
+            self.net = DQN(84, 84, 4, self.hiddenSize, self.actionNum).to(device)
+            self.targetNet = DQN(84, 84, 4, self.hiddenSize, self.actionNum).to(device)
         self.optimizer = optim.Adam(params=self.net.parameters(), lr=self.lr)
 
     def getAction(self, state, ifEpsilonGreedy=True):
@@ -57,9 +232,10 @@ class Agent:
     
     def epsilonDecay(self, N):
         # if N >= 50000:
-        self.epsilon = self.EPS_END + (self.EPS_START - self.EPS_END) * math.exp(-1.0 * N / self.EPS_DECAY)
+        self.epsilon -= (self.EPS_START - self.EPS_END) / self.EPS_DECAY
+        self.epsilon = max(self.epsilon, self.EPS_END)
 
-    def optimzeDQN(self, buf):
+    def learn(self, buf, algo):
         if len(buf) < self.batchSize:
             print("Can't fetch enough exp!")
             return
@@ -71,35 +247,21 @@ class Agent:
         nextStateBatch = torch.cat(batch.nextState).to(device) # batchSize * stateSpace.shape[0]
         doneBatch = torch.cat(batch.done).to(device)  # batchSize * 1
 
-        Q = self.net(stateBatch).gather(1, actionBatch)  # get Q(s_t, a)
-        targetQ = self.targetNet(nextStateBatch).max(1)[0].view(-1, 1) # max_a Q'(s_t+1, a)
-        y = (targetQ * self.gamma) * doneBatch + rewardBatch
-        loss = F.mse_loss(Q, y)
-        estQMean = np.mean(targetQ.detach().cpu().numpy())
-
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-        return loss.item(), estQMean
-    
-    def optimzeDDQN(self, buf):
-        if len(buf) < self.batchSize:
-            print("Can't fetch enough exp!")
-            return
-        exps = buf.sample(self.batchSize)
-        batch = Exp(*zip(*exps))  # batch => Exp of batch
-        stateBatch = torch.cat(batch.state).to(device) # batchSize * stateSpace.shape[0]
-        actionBatch = torch.cat(batch.action).to(device) # batchSize * 1
-        rewardBatch = torch.cat(batch.reward).to(device) # batchSize * 1
-        nextStateBatch = torch.cat(batch.nextState).to(device) # batchSize * stateSpace.shape[0]
-        doneBatch = torch.cat(batch.done).to(device)  # batchSize * 1
-
-        Q = self.net(stateBatch).gather(1, actionBatch)  # get Q(s, a)
-        targetAction = self.net(nextStateBatch).max(1)[1].view(-1, 1)  # get argmax Q'(s_t+1, a)
-        targetQ = self.targetNet(nextStateBatch).gather(1, targetAction)
-        y = (targetQ * self.gamma) * doneBatch + rewardBatch
-        loss = F.mse_loss(Q, y)
-        estQMean = np.mean(targetQ.detach().cpu().numpy())
+        if algo == 'DQN':
+            Q = self.net(stateBatch).gather(1, actionBatch)  # get Q(s_t, a)
+            targetQ = self.targetNet(nextStateBatch).max(1)[0].view(-1, 1) # max_a Q'(s_t+1, a)
+            y = (targetQ * self.gamma) * doneBatch + rewardBatch
+            loss = F.mse_loss(Q, y)
+            estQMean = np.mean(targetQ.detach().cpu().numpy())
+        elif algo == 'DDQN':
+            Q = self.net(stateBatch).gather(1, actionBatch)  # get Q(s, a)
+            targetAction = self.net(nextStateBatch).max(1)[1].view(-1, 1)  # get argmax Q'(s_t+1, a)
+            targetQ = self.targetNet(nextStateBatch).gather(1, targetAction)
+            y = (targetQ * self.gamma) * doneBatch + rewardBatch
+            loss = F.mse_loss(Q, y)
+            estQMean = np.mean(targetQ.detach().cpu().numpy())
+        else:
+            assert(0)
 
         self.optimizer.zero_grad()
         loss.backward()
@@ -117,11 +279,12 @@ class Agent:
             score = 0.0
             # rewardDecay = 1.0
             startTime = time.time()
-            state = env.reset()
+            state = np.array(env.reset())
             for t in count():
-                #env.render()
+                env.render()
                 action = self.getAction(resize(state).unsqueeze(0).to(device))
                 nextState, reward, done, _ = env.step(action.item())
+                nextState = np.array(nextState)
                 buf.push(resize(state).unsqueeze(0).to("cpu"),
                           action.to("cpu"),
                           torch.tensor(np.array([reward]).reshape(1, -1), device="cpu", dtype=torch.float),
@@ -131,19 +294,13 @@ class Agent:
                 # score += rewardDecay * reward
                 # rewardDecay *= self.gamma
                 score += reward
-                if algo == "DQN":
-                    loss, mean = self.optimzeDQN(buf)
-                elif algo == "DDQN":
-                    loss, mean = self.optimzeDDQN(buf)
-                elif algo == "DDDQN":
-                    print("Developing")
-                else:
-                    assert(0)
-                losses.append(loss)
-                Qmeans.append(mean)
+                totalN += 1
+                if totalN % self.learnStride == 0:
+                    loss, mean = self.learn(buf, algo)
+                    losses.append(loss)
+                    Qmeans.append(mean)
                 if ifDecay:
                     self.epsilonDecay(totalN)
-                totalN += 1
                 if totalN % self.updateStride == 0:
                     self.targetNet.load_state_dict(self.net.state_dict())
                 # if done or t + 1 >= self.maxStep:
@@ -184,10 +341,11 @@ class ReplayBuffer:
         if initLen > self.capacity:
             return
         while len(self.buffer) < initLen:
-            state = env.reset()
+            state = np.array(env.reset())
             for t in count():
                 action = env.action_space.sample()
                 nextState, reward, done, _ = env.step(action)
+                nextState = np.array(nextState)
                 self.push(resize(state).unsqueeze(0).to("cpu"),
                           torch.tensor(np.array([action]).reshape(1, -1), device="cpu", dtype=torch.long),
                           torch.tensor(np.array([reward]).reshape(1, -1), device="cpu", dtype=torch.float),
@@ -223,6 +381,40 @@ class DQN(nn.Module):
         x = F.leaky_relu(self.fc4(x.view(x.size(0), -1)))
         return self.head(x)
 
+class DuelingDQN(nn.Module):
+    def __init__(self, h, w, inputChannel, hiddenSize, outputSize):
+        super(DuelingDQN, self).__init__()
+        self.conv1 = nn.Conv2d(inputChannel, 16, 8, 4)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, 4, 2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, 3, 1)
+        self.bn3 = nn.BatchNorm2d(32)
+        def conv2d_size_out(size, kernel_size, stride):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w, 8, 4), 4, 2), 3, 1)
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h, 8, 4), 4, 2), 3, 1)
+        linear_input_size = convw * convh * 32
+        self.fc4_adv = nn.Linear(linear_input_size, hiddenSize)
+        self.fc4_val = nn.Linear(linear_input_size, hiddenSize)
+        self.head_adv = nn.Linear(hiddenSize, outputSize)
+        self.head_val = nn.Linear(hiddenSize, 1)
+
+        self.outputSize = outputSize
+
+    def forward(self, x):
+        x = x / 255
+        x = F.leaky_relu(self.bn1(self.conv1(x)))
+        x = F.leaky_relu(self.bn2(self.conv2(x)))
+        x = F.leaky_relu(self.bn3(self.conv3(x)))
+        x = x.view(x.size(0), -1)
+        adv = F.leaky_relu(self.fc4_adv(x))
+        val = F.leaky_relu(self.fc4_val(x))
+        adv = self.head_adv(adv)
+        val = self.head_val(val).expand(x.size(0), self.outputSize)
+        x = val + adv - adv.mean(1).unsqueeze(1).expand(x.size(0), self.outputSize)
+        return x
+
 # def testNet(env, method):
 #     net = DQN(2, 256, 3)
 #     net.load_state_dict(torch.load('net.pkl'))
@@ -242,7 +434,7 @@ class DQN(nn.Module):
 #     print(avg / 1000)
     
 def main():
-    env = gym.make("BreakoutNoFrameskip-v4")
+    env = getEnv("BreakoutNoFrameskip-v4")
     buf = ReplayBuffer(capacity=100000)
     buf.fill(env, initLen=1000, maxSteps=200)
     agt = Agent(env.action_space,
@@ -253,9 +445,11 @@ def main():
                 batchSize=32,
                 lr=1e-4,
                 hiddenSize=256,
-                updateStride=20,
-                maxStep=1000)
-    agt.DeepQLearning(env, buf, episodeNum=10000, ifDecay=True)
+                learnStride=4,
+                updateStride=10000,
+                maxStep=1000,
+                isDueling=True)
+    agt.DeepQLearning(env, buf, episodeNum=20000, algo='DDQN', ifDecay=True)
     # agt.DoubleDeepQLearning(env, buf, episodeNum=5000, ifDecay=True)
     # draw(5000, 'DDQN', '', 50)
     # testNet(env, 'DDQN')
--- Final/test.py
+++ Final/test.py
@@ -12,10 +12,11 @@ import torch.multiprocessing as mp
 from itertools import count
 import A3C
 import DDPG
+import time
 
 def testDDPG(env):
     net = DDPG.Actor(env.observation_space.shape[0], 512, env.action_space.shape[0], env.action_space.high[0])
-    net.load_state_dict(torch.load('DDPG/Hopper2/netDDPG_A.pkl'))
+    net.load_state_dict(torch.load('DDPG/HalfCheetah1/netDDPG_A1.pkl'))
     for i in range(100):
         state = env.reset()
         for t in count():
@@ -23,9 +24,12 @@ def testDDPG(env):
             action = net.forward(torch.tensor(state.reshape(1, -1), dtype=torch.float))
             nextState, reward, done, _ = env.step(action.detach().numpy())
             state = nextState
+            time.sleep(0.2)
+            if t + 1 >= 200:
+                done = True
             if done:
                 print("Episode %d ended in %d steps" % (i + 1, t + 1))
                 break
 
-env = gym.make('Hopper-v2')
+env = gym.make('HalfCheetah-v2')
 testDDPG(env)
